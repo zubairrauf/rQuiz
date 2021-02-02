10a2d2b22e0e4ec9ac39122ff977a7b837cb09bc
Zubair <zubairrr@gmail.com>
2021-02-02 09:01:48 +0100

    Added some styles to the Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,29 @@
-import { Link } from 'react-router-dom'
-import Helmet from 'react-helmet'
-import CarIcon from 'mdi-react/CarIcon';
+import { Link } from "react-router-dom";
+import Helmet from "react-helmet";
+import CarIcon from "mdi-react/CarIcon";
 
 const Home = () => (
-    <>
-        <Helmet><title>Quiz App - Home</title> </Helmet>
-        <div className = 'container'>
-            <div>
-                <CarIcon className='icon_car' size={60}/>
-            </div>
-            <h1>Quiz App</h1>
-            <ul className='buttons'>
-                <li className='button'><Link to={'/play'}>Play</Link></li>
-                <li className='button'><Link to={'/instructions'}>Instructions</Link></li>
-            </ul>
+  <>
+    <Helmet>
+      <title>Quiz App - Home</title>{" "}
+    </Helmet>
+    <div className="container">
+      <div className="icon-container">
+        <CarIcon className="icon_car" size={60} />
+      </div>
+      <div className="inner-container">
+        <h1>Quiz App</h1>
+        <ul className="buttons">
+          <li className="button">
+            <Link to={"/play"}>Play</Link>
+          </li>
+          <li className="button">
+            <Link to={"/instructions"}>Instructions</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </>
+);
 
-        </div>
-    </>
-)
-
-export default Home
+export default Home;
